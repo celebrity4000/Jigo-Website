197fe39aeb602b80d835530629db9eb63d2e1652
Darkrider0007 <rg2512@it.jgec.ac.in>
2025-02-14 16:42:50 +0530

    refactor: enhance popup component functionality and styling; add close button

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,36 +1,40 @@
 import { useState, useEffect } from "react";
-import { Modal, Button } from "react-bootstrap";
-import popup1 from "../assets/popup1.jpg"
-import popup2 from "../assets/popup2.jpg"
+import { Modal } from "react-bootstrap";
+import popup1 from "../assets/popup1.jpg";
+import popup2 from "../assets/popup2.jpg";
+import { useLocation } from "react-router-dom";
 
 const Popup = () => {
     const [show, setShow] = useState(false);
     const [Image, setImage] = useState('');
+    const location = useLocation();
 
     useEffect(() => {
         const random = Math.floor(Math.random() * 2);
-        if (random === 0) {
-            setImage(popup2);
-        } else {
-            setImage(popup1);
-        }
-        popup2
-    }, []);
+        setImage(random === 0 ? popup2 : popup1);
+    }, [location]);
 
     useEffect(() => {
-        setShow(true); // Show popup on load
-    }, []);
+        const timer = setTimeout(() => {
+            setShow(true);
+            setTimeout(() => setShow(false), 7000);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [location]);
 
     const handleClose = () => setShow(false);
 
     return (
-        <Modal show={show} onHide={handleClose} centered>
-            <Modal.Body className="text-center" onClick={handleClose}>
-                <img
-                    src={Image}
-                    alt="Popup Image"
-                    className="img-fluid rounded"
-                />
+        <Modal show={show} centered onHide={handleClose}>
+            <Modal.Body className="text-center position-relative">
+                <button
+                    onClick={handleClose}
+                    className="position-absolute top-0 end-0 m-2 border-0 bg-transparent text-dark"
+                    style={{ fontSize: "1.5rem", cursor: "pointer" }}
+                >
+                    ✖
+                </button>
+                <img src={Image} alt="Popup Image" className="img-fluid rounded" />
             </Modal.Body>
         </Modal>
     );
